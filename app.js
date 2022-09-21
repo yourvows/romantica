b595d78b999e83c5d86719d1e83974a3ba564079
IBOBRO <ibrohimlogos@gmail.com>
2022-09-22 03:37:10 +0500

    realise

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var b = document.querySelector("button");
-b.addEventListener("click",change);
-function change()
-{
-    var i = Math.floor(Math.random()*500)+1;
-    var j = Math.floor(Math.random()*500)+1;
-    b.style.left = i+"px";
-    b.style.top = j+"px";
+var text = "Тогда нажми на сердечку)";
+var b = document.querySelector("input");
+b.addEventListener("click", change);
+function change() {
+  
+  var i = Math.floor(Math.random() * 100) + 1;
+  var j = Math.floor(Math.random() * 500) + 1;
+  var p = j - 50;
+  b.style.left = i + "px";
+  b.style.top = p + "px";
+  console.log(i);
+  console.log(j);
+}
+var input = document.querySelector("#noid")
+input.addEventListener("click", abortHandler)
+function abortHandler() {
+  console.log('fsafsd')
+  var audio = document.getElementById("audio");
+  audio.play();
+  alert(text)
 }

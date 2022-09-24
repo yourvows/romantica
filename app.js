735855c66b94173ddf1184ce32d318e25cff7450
IBOBRO <ibrohimlogos@gmail.com>
2022-09-25 04:01:43 +0500

    fixed all bugs and buttons

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var text = "Тогда нажми на сердечку)";
+var text = "Подними громкость \n Нажми на сердечку)";
 var b = document.querySelector("input");
 b.addEventListener("click", change);
 function change() {
-  
+
   var i = Math.floor(Math.random() * 100) + 1;
   var j = Math.floor(Math.random() * 500) + 1;
   // var p = j - 50;
-  
+
   b.style.left = i + "px";
   b.style.top = j + "px";
   // b.style.top = p + "px";
 
-  if(i>200){
+  if (i > 200) {
     b.style.left = 150 + "px";
   }
-  else{
+  else {
     b.style.left = i + "px";
   }
-  if(j>260){
+  if (j > 260) {
     b.style.top = 155 + "px";
   }
-  else{
+  else {
     b.style.top = j + "px";
   }
   console.log(i);
